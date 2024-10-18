5ecfe3fe4320f44510d8481eed6bb38170a394d1
Alakh Chaturvedi <alakh1304@gmail.com>
2024-10-18 11:39:59 +0530

    Routing and Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'books',
+  loadChildren: () => import('./components/book-list/book-list.module').then(m => m.BookListModule)
+},
+{
+  path: 'add-book',
+  loadChildren: () => import('./components/book-form/book-form.module').then(m => m.BookFormModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
