56165e2ae345ba62da050271fe38ae34b7fd2574
Alakh Chaturvedi <alakh1304@gmail.com>
2024-10-18 11:42:02 +0530

    Implement Forms and Reactive Programming Using RxJS

--- src/app/components/book-form/book-form.component.ts
+++ src/app/components/book-form/book-form.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-book-form',
   templateUrl: './book-form.component.html',
   styleUrls: ['./book-form.component.css']
 })
+
 export class BookFormComponent {
-  book = { title: '', author: '', genre: '' };
+  bookForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private bookService: BookService) {
+    this.bookForm = this.fb.group({
+      title: [''],
+      author: [''],
+      genre: ['']
+    });
+  }
 
   submitForm() {
-    // Logic to add/edit a book
-    console.log(this.book);
+    this.bookService.addBook(this.bookForm.value).subscribe(response => {
+      console.log('Book added!', response);
+    });
   }
 }
