cb11124d937fbcf78e9165a292bd41666c064689
Alakh Chaturvedi <alakh1304@gmail.com>
2024-10-18 11:28:57 +0530

    Display the list of books

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  books = [];
 
+  ngOnInit(): void {
+    // Fetch book data (static for now)
+    this.books = [
+      { title: 'Book 1', author: 'Author 1', genre: 'Fiction' },
+      { title: 'Book 2', author: 'Author 2', genre: 'Non-fiction' }
+    ];
+  }
 }
