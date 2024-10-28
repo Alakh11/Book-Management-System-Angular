6672f12856172043523ce51a862e8436a1ded2d2
Alakh Chaturvedi <alakh1304@gmail.com>
2024-10-28 15:51:00 +0530

    Add lazy loading feature for Book components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,18 @@ import { BookListComponent } from './components/book-list/book-list.component';
 
 const routes: Routes = [
 { path: '', redirectTo: '/books', pathMatch: 'full' }, // Redirect to empty path to default route
+{
+  path: 'book-list',
+  loadChildren: () => import('src/app/components/book-list/book-list.module').then(m => m.BookListModule)
+},
+{
+  path: 'book-form',
+  loadChildren: () => import('src/app/components/book-form/book-form.module').then(m => m.BookFormModule)
+},
+{ path: '', redirectTo: '/book-list', pathMatch: 'full' },
 { path: 'books', component: BookListComponent }, // Define your routes
 { path: 'add-book', component: BookFormComponent }, 
 { path: '**', redirectTo: '/books' },// Wildcard route to catch undefined routes
-//{ loadChildren: () => import('./components/book-form/book-form.module').then(m => m.BookFormModule) }
 ];
 
 @NgModule({
