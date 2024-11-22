0d0f567a5c4d4e2fd738b74838b1d9f1ec7afc6f
Alakh Chaturvedi <alakh1304@gmail.com>
2024-11-22 23:41:50 +0530

    test cases

--- src/app/components/book-list/book-list.component.spec.ts
+++ src/app/components/book-list/book-list.component.spec.ts
@@ -1,21 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BookListComponent } from './book-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 describe('BookListComponent', () => {
-  let component: BookListComponent;
-  let fixture: ComponentFixture<BookListComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [BookListComponent]
-    });
-    fixture = TestBed.createComponent(BookListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BookListComponent],
+      imports: [HttpClientModule], // Add this line
+    }).compileComponents();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(BookListComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
